ee71f37b8c63e81c977b8b48e3c18426ee901755
Thierno Barry <thierno05@hotmail.com>
2023-03-07 13:01:14 -0500

    first attempt at fixing the persisting store

--- linkedin-clone/src/store/index.js
+++ linkedin-clone/src/store/index.js
@@ -1,7 +1,19 @@
 import {createStore, applyMiddleware} from "redux"
 import thunkMiddleware from "redux-thunk"
 import rootReducer from "../reducers"
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
 
-export default store
+//const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+export const store = createStore(persistedReducer, {} ,applyMiddleware(thunkMiddleware))
+export  const persistor = persistStore(store)
