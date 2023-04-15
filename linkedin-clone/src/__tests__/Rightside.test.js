052919ec5c3ffdfaf54649eb33bf53d7acba1507
nicotowa <nicolastowakamgne@gmail.com>
2023-04-15 17:15:20 -0400

    s5 first commit

--- linkedin-clone/src/__tests__/Rightside.test.js
+++ linkedin-clone/src/__tests__/Rightside.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Rightside from '../components/Rightside';
 
 test('renders the Rightside component', () => {
-  render(
+  createRoot(
     <Rightside />);
   const linkElement = screen.getByText("Add to your feed");
   expect(linkElement).toBeInTheDocument();
